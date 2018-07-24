db988851b639ae3b82d784ef43df18cc3eac6821
Samwell61 <samuel.bailey1@gmail.com>
2018-07-24 21:31:01 +0100

    Reading and writing working

--- Heating/Config.cpp
+++ Heating/Config.cpp
@@ -14,42 +14,32 @@ Config::Config(const char* fn, char separator) : separator(separator) {
 }
 
 bool Config::writeProperty(const char* property, const char* value) {
-	Serial.println(fileName);
-	Serial.println(SD.exists(fileName) ? "Config file found" : "Config file not found");
 	byte valueLength = strlen(value);
 	if ((strlen(property) > MAX_PROPERTY_LENGTH) || (valueLength > MAX_VALUE_LENGTH))
 		return 0; // Failed to write value
-	File file = openFile();
+	File file = SD.open(fileName, O_READ | O_WRITE);
+	file.seek(0); // Go to start of file (FILE_WRITE defaults to end of file)
 	char currentChar;
 	char currentProperty[MAX_PROPERTY_LENGTH + 1];
+	currentProperty[0] = '\0';
 	currentValue[0] = '\0'; // Reset the current value, ready for new search
 	while ((currentChar = file.read()) > -1) {
-		Serial.print(currentChar);
 		if (currentChar == separator) { // If at end of current property, check against requested property
-			Serial.println(" At separator ");
 			if (strncmp(currentProperty, property, strlen(currentProperty)) == 0) { // Compare current property and the requested property
-				Serial.println("Found property");
 				// Pointer will currently be after the separator
 				writeValue(file, value);
 				file.close();
 				return 1; // Value has been written
 			}
 			else {
-				Serial.println("Incorrect property");
 				currentProperty[0] = '\0'; // Reset to get next property
 				while (file.read() != '\n'); // Move pointer to end of line
 			}
 		}
 		else {
 			byte len = strlen(currentProperty);
-			if (len >= MAX_PROPERTY_LENGTH) { // Overload protection
-
-				break;
-			}
-			currentProperty[len + 1] = currentChar; // Add current character to char array
-			currentProperty[len + 2] = '\0'; // Re-terminate the char array
-			Serial.print("Current currentProperty: ");
-			Serial.println(currentProperty);
+			currentProperty[len] = currentChar; // Add current character to char array
+			currentProperty[len + 1] = '\0'; // Re-terminate the char array
 		}
 	}
 	// If program gets to here, property does not currently exist and pointer is at bottom of file
@@ -69,14 +59,13 @@ bool Config::writeProperty(const char* property, const char* value) {
 	// If not, go the the end of the file and print the new name, separator and value
 }
 void Config::writeValue(File &file, const char* value) {
-	file.write(value);
-	for (byte i = 0; i < MAX_VALUE_LENGTH; i++) { // Pad out line to maximum length
-		file.write("\0");
+	file.print(value);
+	for (byte i = strlen(value); i < MAX_VALUE_LENGTH; i++) { // Pad out line to maximum length
+		file.print(" ");
 	}
-	file.write("\n"); // Add newline to end of line
+	file.print("\n"); // Add newline to end of line
 }
 char* Config::readProperty(const char* property) {
-	
 	
 	File file = SD.open(fileName);
 	char currentChar;
@@ -89,16 +78,13 @@ char* Config::readProperty(const char* property) {
 	
 	while ((currentChar = file.read()) > -1) {
 		if (currentChar == separator) { // If at end of current property, check against requested property
-			
 			if (strncmp(currentProperty, property, strlen(currentProperty)) == 0) { // Compare current property and the requested property
-				
 				while ((currentChar = file.read()) != '\n') {
 					byte len = strlen(currentValue);
 					currentValue[len] = currentChar;
 					currentValue[len + 1] = '\0';
 				}
 				file.close(); // Close file after finding value
-				
 				return currentValue; // Return value
 			}
 			else {
