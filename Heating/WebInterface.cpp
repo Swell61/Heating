8b233bb7817d815316aca764bc0d20b80a957848
Samwell61 <samuel.bailey1@gmail.com>
2018-07-24 14:21:35 +0100

    Removed unrequired loop from constuctor

--- Heating/WebInterface.cpp
+++ Heating/WebInterface.cpp
@@ -8,20 +8,13 @@ WebInterface::WebInterface(bool SDAvailable) : webFilesAvailable(SDAvailable) {
 	Ethernet.begin(mac, ip);
 	
 	ip = Ethernet.localIP();
-	for (byte thisByte = 0; thisByte < 4; thisByte++) {
-		// print the value of each byte of the IP address:
-		
-		
-	}
-	
-
 	// start listening for clients
 	server.begin();
 }
 
 void WebInterface::processRemoteOutput(int time, byte heatingMode, byte waterMode, float temp, bool heatingStatus, bool waterStatus, float requestedTemp, bool heatingBoost, bool waterBoost) {
 	// Send a string containing the status of the main display
-	Serial.println("Sending message 1 to client");
+	
 	char buffer[8];
 	itoa(time, buffer, 10);
 	char output[33] = "0:";
@@ -58,18 +51,18 @@ void WebInterface::processRemoteOutput(int time, byte heatingMode, byte waterMod
 	strcat(output, "\0");
 	strcat(buffer, "\0");
 
-	Serial.println("Message ready");
-	Serial.print("Buffer: ");
-	Serial.println(buffer);
-	Serial.print("Output: ");
-	Serial.println(output);
+	
+	
+	
+	
+	
 	webServerStack_ProcessMsgOut(output);
-	Serial.println("Message sent");
+	
 }
 void WebInterface::processRemoteOutput(bool heatingTimerStatus, bool waterTimerStatus, int heatingOnMorning, int heatingOffMorning, int heatingOnAfternoon, int heatingOffAfternoon, int waterOnMorning, int waterOffMorning, int waterOnAfternoon, int waterOffAfternoon) {
 	// Send a string containing the status of the timer
 	//String output = "1:" + (String)heatingTimerStatus + ":" + (String)waterTimerStatus + ":" + (String)heatingOnMorning + ":" + (String)heatingOffMorning + ":" + (String)heatingOnAfternoon + ":" + (String)heatingOffAfternoon + ":" + (String)waterOnMorning + ":" + (String)waterOffMorning + ":" + (String)waterOnAfternoon + ":" + (String)waterOffAfternoon;
-	Serial.println("Sending message 2 to client");
+	
 	char buffer[6];
 	char output[72] = "1:";
 	
@@ -111,9 +104,8 @@ void WebInterface::processRemoteOutput(bool heatingTimerStatus, bool waterTimerS
 	strcat(output, buffer);
 	strcat(output, "\0");
 	strcat(buffer, "\0");
-	Serial.println("Message ready");
-	Serial.println(buffer);
-	Serial.println(output);
+	
+	
 	webServerStack_ProcessMsgOut(output);
 }
 
@@ -130,19 +122,19 @@ int WebInterface::webServerStack_ProcessMsgIn() {
 		// Process for connected client
 		for (int i = 0; i < MAX_CLIENT_NUM; i++) {
 			if (webSocketStack[i].client) {
-				Serial.println("Got client");
 				if (webSocketStack[i].client.connected()) {
-					Serial.println("Client connected");
 					// Incoming msg
 					if (webSocketStack[i].client.available()) {
-						Serial.println("Client sending data");
 						const char *data = webSocketStack[i].webSocketServer.getData();
-						Serial.println("Got data");
 						if (isDigit(data[0]) && (strlen(data) == 1 || (strlen(data) == 2 && isDigit(data[1])))) { // If they have sent a valid message
 							
 							
 							return atoi(data); // Return it
 						}
+						else {
+							webSocketStack[i].client.stop(); // IE and Edge send rubbish through websocket when refreshing or closing. Cick the client if non-valid data sent
+						}
+						
 						inMsgPassed = true;
 						return 0;
 					}
@@ -175,18 +167,14 @@ int WebInterface::webServerStack_ProcessMsgIn() {
 		if (j < MAX_CLIENT_NUM) { // If there is room on the stack for another client
 			webSocketStack[j].client = server.available();
 			if (webSocketStack[j].client.connected()) {
-				Serial.println("Getting request type");
 				byte request = webSocketStack[j].webSocketServer.handshake(webSocketStack[j].client); // try to handshake. Will return 2 if web files need to be sent
 				
 				if (request == 1) { // If handshake succeeded
-					Serial.println("Handshake");
 					return 255;
 				}
 				else if (request == 2 && webFilesAvailable) { // If need to send web files and web files are available
-					Serial.println("Web files. Getting request path");
 					char* requestPath = webSocketStack[j].webSocketServer.getRequestPath(); // Get the requested file path
 					if (strlen(requestPath) == 0) { // If client requests an empty path, send them the main page HTML file
-						Serial.println("Empty request");
 						strcpy(requestPath, "index.htm");
 					}
 					if (SD.exists(requestPath)) { // if the file is available
@@ -239,11 +227,9 @@ int WebInterface::webServerStack_ProcessMsgIn() {
 }
 
 void WebInterface::webServerStack_ProcessMsgOut(const char *output) {
-	Serial.println(freeMemory());
-	Serial.println("Processing message out");
+	
 	for (int i = 0; i < MAX_CLIENT_NUM; i++) { // Loop through each client connected
-		Serial.print("Client: ");
-		Serial.println(i);
+		
 		if (webSocketStack[i].client) {
 			if (webSocketStack[i].client.connected()) {
 				
@@ -255,8 +241,6 @@ void WebInterface::webServerStack_ProcessMsgOut(const char *output) {
 			}
 		}
 	}
-	Serial.println(freeMemory());
-	Serial.println("Finished sending");
 }
 
 void WebInterface::sendClientData(int id, const char *s) {
